28093e84fb8ac8a47c5bf2018470a71c4d359ae3
Rupesh Prajapati <39094963+Rupeshpra22@users.noreply.github.com>
2022-05-05 19:32:31 +0530

    passed function to the context provider

--- src/App.js
+++ src/App.js
@@ -28,14 +28,17 @@ function App() {
   };
 
   return (
-    <AuthContext.Provider value={{isLoggedIn: isLoggedIn}}>
-
-      <MainHeader onLogout={logoutHandler} />
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+      }}
+    >
+      <MainHeader />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    
     </AuthContext.Provider>
   );
 }
