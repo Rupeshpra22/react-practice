744b3d79a2b6fd158e934ca48526f0c8d6dcbdf6
Rupesh Prajapati <39094963+Rupeshpra22@users.noreply.github.com>
2022-05-05 19:09:21 +0530

    implemented context using provider and consumer approach

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
-import {useEffect} from 'react';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
+import { useEffect } from "react";
+import AuthContext from "./store/auth-context";
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  useEffect(()=>{
-    const storedData = localStorage.getItem('isLoggedIn')
-    if(storedData === '1'){
+  useEffect(() => {
+    const storedData = localStorage.getItem("isLoggedIn");
+    if (storedData === "1") {
       setIsLoggedIn(true);
     }
-  }, [])
+  }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
-    localStorage.setItem('isLoggedIn','1');
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
@@ -27,13 +28,15 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider value={{isLoggedIn: isLoggedIn}}>
+
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    
+    </AuthContext.Provider>
   );
 }
 
